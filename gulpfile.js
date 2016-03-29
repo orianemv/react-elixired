bec1ae48e89a41251574093535f9e1be588aee47
Amine El Ghayate <mhwebdev@gmail.com>
2016-03-29 12:02:33 +0200

    initial

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var elixir = require('laravel-elixir');
+/**
+ destination folder
+ **/
+var dest = 'public/';
+
+//trailing slash required.
+elixir.config.assetsDir = 'resources/assets/'; 
+
+elixir.config.publicPath = dest;
+
+elixir.config.js.browserify.transformers.push({
+    name: 'babelify',
+    options: {}
+});
+
+
+elixir(function(mix) {
+    mix.sass('app.scss', dest + 'css/style.css');
+});
+
+elixir(function(mix) {
+    mix.browserify('main.js', dest + 'js/bundle.js', null, {
+        global: true
+    });
+});
+
+elixir(function(mix) {
+    mix.browserSync({
+      server:'./public',
+      proxy: false
+    });
+});
